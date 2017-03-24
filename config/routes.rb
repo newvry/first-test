1e6f7999a8118db450ff01e0be2782537532728c
LYC <newvry@hotmail.com>
2017-03-24 18:20:09 +0800

    Nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :informations
+  resources :informations do
+    resources :comments
+  end
   root "informations#index"
 
 end
