017a95ffa93ac579745673c3a8d196857446df68
LYC <newvry@hotmail.com>
2017-03-24 18:19:05 +0800

    create comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324080029) do
+ActiveRecord::Schema.define(version: 20170324093137) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "comment"
+    t.integer  "information_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "information", force: :cascade do |t|
