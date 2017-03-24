e4e081646014e6ad454cd928e4da4884975f8717
LYC <newvry@hotmail.com>
2017-03-24 17:06:50 +0800

    CRUD finish

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -17,12 +17,35 @@ class InformationsController < ApplicationController
     end
   end
 
+  def show
+   @information = Information.find(params[:id])
+  end
+
+  def edit
+   @information = Information.find(params[:id])
+  end
+
+  def update
+    @information = Information.find(params[:id])
+    if @information.update(information_params)
+      redirect_to informations_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+   @information = Information.find(params[:id])
+   @information.destroy
+
+   redirect_to informations_path
+  end
 
 
   private
 
   def information_params
-   params.require(:information).permit(:title, :content)
+   params.require(:information).permit(:title, :content, :category_id)
   end
 
 end
